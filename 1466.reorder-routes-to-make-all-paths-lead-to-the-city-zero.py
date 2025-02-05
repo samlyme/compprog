b0d0a9aee164a11725a35ff5bc30381201f79260
Sam Ly <sam@samly.me>
2025-02-04 17:48:00 -0800

    1466. 77/77 cases passed (163 ms) Your runtime beats 87.73 % of python3 submissions Your memory usage beats 35.28 % of python3 submissions (52.3 MB)

--- 1466.reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1466.reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -5,34 +5,36 @@
 #
 
 # @lc code=start
-from collections import defaultdict, deque
+from collections import deque
 from typing import List
 
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        graph = [[0] * n for _ in range(n)]
+        adj_list = [set() for _ in range(n)]
+        inv_list = [set() for _ in range(n)]
        
         for init, term in connections:
-            graph[init][term] = 1
+            adj_list[init].add(term)
+            inv_list[term].add(init)
 
         # is a tree with root zero
         # if you can reach a node from zero, then you must reorder the path
         count = 0
         frontier = deque([0])
+        visited = [False] * n
 
         while frontier:
             curr = frontier.pop()
+            visited[curr] = True
             
-            for j in range(n):
-                if curr == j: continue
-                
-                if graph[curr][j] == 1:
+            for next in adj_list[curr]:
+                if not visited[next]:
                     count += 1
-                    frontier.append(j)
-                    graph[curr][j] = 0
-                elif graph[j][curr] == 1:
-                    frontier.append(j)
-                    graph[j][curr] = 0
+                    frontier.append(next)
+
+            for next in inv_list[curr]:
+                if not visited[next]:
+                    frontier.append(next)
 
         return count
         
