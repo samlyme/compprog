b46f5dcffaa898c3dc32c7e44f6929388b17b09d
Sam Ly <sam@samly.me>
2025-02-18 00:59:49 -0800

    194/194 cases passed (58 ms) Your runtime beats 76.24 % of python3 submissions Your memory usage beats 62.55 % of python3 submissions (19.3 MB)

--- leetcode/1926.nearest-exit-from-entrance-in-maze.py
+++ leetcode/1926.nearest-exit-from-entrance-in-maze.py
@@ -11,21 +11,37 @@ from typing import List
 
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
-        DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        depth = 1
-        frontier = deque([(entrance[0], entrance[1])])
-        explored = set()
-        
-        while frontier:
-            for _ in range(len(frontier)):
-                curr = frontier.popleft()
+        m = len(maze)
+        n = len(maze[0])
+        if m == 1 and n == 1:
+            return -1
 
-                for dir in DIRECTIONS:
-                    n = tuple(a + b for a, b in zip(curr, dir))
-                    if 
-                
+        depth = 0
+        queue = deque([(entrance[0], entrance[1])])  # step 1
+        maze[entrance[0]][entrance[1]] = "+"  # step 2
+
+        while queue:
+
+            for _ in range(len(queue)):
+                row, col = queue.popleft()  # step 3
+                for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:  # step 4
+                    nr, nc = row + dy, col + dx
+                    # step 5
+                    if 0 <= nr < m and 0 <= nc < n and maze[nr][nc] == ".":
+                        if (nr == 0 or nr == m - 1) or (nc == 0 or nc == n - 1):
+                            return depth + 1
+
+                        maze[nr][nc] = "+"
+                        queue.append((nr, nc))
 
             depth += 1
-        return -1
-# @lc code=end
 
+        return -1
+
+    def isExit(self, maze: List[List[str]], coord: tuple[int, int]) -> bool:
+        if maze[coord[0]][coord[1]] == "+":
+            return False
+
+        return (coord[0] == 0 or coord[0] == len(maze) - 1
+                or coord[1] == 0 or coord[1] == len(maze[0]) - 1)
+# @lc code=end
