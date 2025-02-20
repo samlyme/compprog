266b03c35b22856c581b36c6ee4178e3859fd8a2
Sam Ly <sam@samly.me>
2025-02-19 19:11:23 -0800

    devious one-liner

--- leetcode/1980.find-unique-binary-string.py
+++ leetcode/1980.find-unique-binary-string.py
@@ -10,19 +10,5 @@ from typing import List
 
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        n = len(nums[0])
-        nums.sort()
-        if int(nums[0], 2) != 0:
-            return '0' * n
-
-        if int(nums[-1], 2) < 2 ** n - 1:
-            return str(bin(2 ** n - 1))[2:].rjust(n, '0')
-
-        for i in range(len(nums) - 1):
-            a = int(nums[i], 2)
-            b = int(nums[i+1], 2)
-            if b - a > 1:
-                return str(bin(a+1))[2:].rjust(n, '0')
-
-        return ""
+        return ''.join(['1' if num[i] == '0' else '0' for i, num in enumerate(nums)])
         # @lc code=end
